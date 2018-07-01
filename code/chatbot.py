8ad0e47a186d5dbbf9aa867982b635dd1f5fc562
luis-israel-velez <luis.israel.velez@gmail.com>
2018-07-01 11:23:18 -0700

    New folder with source code.

---
+++ code/chatbot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#All credit to setndex (youtube)
+#Test program to create chatbot based on redit comments for trainning purposes
+
+import sqlite3
+import json
+from datetime import datetime 
+
+print("Hello did the import work?")
